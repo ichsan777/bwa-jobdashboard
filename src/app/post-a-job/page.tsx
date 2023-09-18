09a41e6ed7cdaf60bca9e0796f39963b2230e2fb
ichsan777 <ichsanaminudin8@gmail.com>
2023-09-18 17:58:19 +0700

    slicing sidebar,header, page post-a-job

---
+++ src/app/post-a-job/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface PostJobPageProps {}
+
+const PostJobPage: FC<PostJobPageProps> = ({}) => {
+  return <div>PostJobPage</div>;
+};
+
+export default PostJobPage;
